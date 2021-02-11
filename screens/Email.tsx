05916fab097390fba30cb0d19d310cf0d53e6f20
Pat Weisensee <pat@synergetx.io>
2021-02-11 14:34:07 -0600

    reset scan tabs to clean state after successful email send, cleanup emails placeholder page

--- screens/Email.tsx
+++ screens/Email.tsx
@@ -42,6 +42,8 @@ export default function EmailScreen(props: Props) {
             const success = await sendGridEmail(toAddress, SUBJECT, body);
             setEmailSending(false);
             if (success) {
+                // blow out selected scans
+                props.navigation.navigate('ScanHistory', { selectedScanIds: [] });
                 props.navigation.navigate('Email', { screen: 'EmailHistory' });
                 Toast.show({
                     text1: 'Success! Email sent.',
